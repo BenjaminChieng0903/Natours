f9bb4c56eea3cd7c88417f11555600443627170f
Benjamin Qian <qianjingning0903@gmail.com>
2023-07-05 23:47:02 +1000

    createDatabaseWithMongodb/DatabaseConfig/debugger

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// console.log(process.env);
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASEPASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => {
+    // console.log(connection);
+    console.log('connection is successful!');
+  });
+
 const app = require('./app');
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`server is running on ${port} `);
