d9a914c8c3a7f1cc2dc7efa42de4e876a1c2461f
Benjamin Qian <qianjingning0903@gmail.com>
2023-07-05 23:53:09 +1000

    tourRoutesRefactoring

--- routes/ToursRoutes.js
+++ routes/ToursRoutes.js
@@ -2,12 +2,21 @@ const express = require('express');
 const tourRouter = express.Router();
 const {
   getAlltours,
-  addNewtours,
-  getUserById,
-  deleteUserById,
+  createNewTour,
+  getTourById,
+  deleteTourById,
+  updateTour,
+  getTourStats,
+  CaculatebusinessMonth,
 } = require('./../controllers/ToursController');
 
-tourRouter.route('/').get(getAlltours).post(addNewtours);
-tourRouter.route('/:id').get(getUserById).patch().delete(deleteUserById);
+tourRouter.route('/').get(getAlltours).post(createNewTour);
+tourRouter.route('/get-tour-stats').get(getTourStats);
+tourRouter.route('/businessMonth').get(CaculatebusinessMonth);
+tourRouter
+  .route('/:id')
+  .get(getTourById)
+  .post(updateTour)
+  .delete(deleteTourById);
 
 module.exports = tourRouter;
