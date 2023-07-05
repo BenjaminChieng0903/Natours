c49596a87998576bbbded1d4f9ae6d7005dd8feb
Benjamin Qian <qianjingning0903@gmail.com>
2023-07-05 23:42:25 +1000

    createTourSchema/Model

---
+++ models/tourModels.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  startDates: [Date],
+  images: [String],
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an imageCover'],
+  },
+  CreateAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
