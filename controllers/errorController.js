615d97c7f592ec87135c5ae99b6c4cc53c6b3f5a
Benjamin Qian <qianjingning0903@gmail.com>
2023-07-07 00:05:41 +1000

    castErrorHandler(InvalidID)

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -21,6 +21,14 @@ exports.globalErrorHandler = (err, req, res, next) => {
     }
     //Otherwise if it is a internal error, we don't have to tell client why it is wrong.
     else {
+      //Handle invalid ID, that is castError
+      if (err.name === 'CastError') {
+        res.status(500).json({
+          status: err.status,
+          statusCode: err.statusCode,
+          message: `Invalid ID ${err.stringValue}`,
+        });
+      }
       res.status(500).json({
         status: err.status,
         statusCode: err.statusCode,
