8e230654cbbe80382a416b95ea1d2dc0d10ee124
Benjamin Qian <qianjingning0903@gmail.com>
2023-07-08 02:18:09 +1000

    encryptedPasswordBeforeStorage

--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -35,7 +36,14 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
-
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  console.log(this.get('password'));
+  const cryptedPassword = bcrypt.hash(this.get('password'), 12);
+  this.password = await cryptedPassword;
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
