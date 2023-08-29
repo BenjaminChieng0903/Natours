b5209f5823fcff184150bc976a52a3a9a36c5707
Benjamin Qian <qianjingning0903@gmail.com>
2023-08-30 02:49:16 +1000

    refactorTheReviewPage/RefineUI

--- client/src/components/customerReview/customerReview.jsx
+++ client/src/components/customerReview/customerReview.jsx
@@ -5,6 +5,18 @@ import { selectorBookingsReview } from "../store/booking/booking.selector";
 const CustomerReview = () => {
   const tour = useSelector(selectorBookingsReview);
   const imageUrl = `/img/tours/${tour.imageCover}`;
+  const convertDateFormat = () => {
+    const dateObject = new Date(tour.startDates[0]);
+    const options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      timeZone: "UTC",
+    };
+    const formattedDate = dateObject.toLocaleString("en-AU", options);
+    console.log(formattedDate);
+    return formattedDate;
+  };
   return (
     <main className="main">
       <div className="customer-review">
@@ -19,21 +31,43 @@ const CustomerReview = () => {
         </div>
         <div className="customer-review__body">
           <div className="tour-infomation">
-            <div className="overview-box__group">
+            {/* <div className="overview-box__group">
               <div className="description-box">
                 <h2 className="heading-secondary ma-bt-lg">About</h2>
                 <p className="description__text">{tour.summary}</p>
               </div>
-            </div>
+            </div> */}
             <div className="overview-box__group">
-              <h2 className="heading-secondary ma-bt-lg">Quick facts</h2>
+              <h2 className="heading-secondary ma-bt-lg">Your Tour Info</h2>
+              <div className="overview-box__detail">
+                <svg className="card__icon">
+                  <use href="/img/icons.svg#icon-flag"></use>
+                </svg>
+                <span className="overview-box__label">Stops</span>
+                <span style={{ fontWeight: "bold" }}>
+                  {tour.locations.length} stops
+                </span>
+              </div>
+              <div className="overview-box__detail">
+                {/* <svg className="card__icon">
+                  <use href="/img/icons.svg#icon-flag"></use>
+                </svg>
+                <span className="overview-box__label">Stops</span> */}
+                {tour.locations.map((stop) => {
+                  return (
+                    <span style={{ fontWeight: "bold" }}>
+                      day {stop.day}: {stop.description}
+                    </span>
+                  );
+                })}
+              </div>
               <div className="overview-box__detail">
                 <svg className="overview-box__icon">
                   <use href="/img/icons.svg#icon-calendar"></use>
                 </svg>
                 <span className="overview-box__label">Your date</span>
                 <span className="overview-box__text">
-                  {/* {convertDateFormat()} */}
+                  {convertDateFormat()}
                 </span>
               </div>
               <div className="overview-box__detail">
